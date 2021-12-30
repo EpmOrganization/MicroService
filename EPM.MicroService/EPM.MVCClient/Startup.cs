257cc40bc3c2d8856bdf2938ad71d9afa93bb41c
jiangxl <jiangxl@hicore.com>
2021-12-30 11:10:13 +0800

    添加Polly处理熔断和降级

--- EPM.MicroService/EPM.MVCClient/Startup.cs
+++ EPM.MicroService/EPM.MVCClient/Startup.cs
@@ -1,5 +1,6 @@
 using EPM.Core.Discovery;
 using EPM.Core.HttpClientConsul.Extentions;
+using EPM.Core.HttpClientPolly;
 using EPM.Core.ServiceBase;
 using EPM.Model.ConfigModel;
 using Microsoft.AspNetCore.Builder;
@@ -11,6 +12,8 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace EPM.MVCClient
@@ -27,6 +30,13 @@ namespace EPM.MVCClient
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // 1、自定义异常处理(用缓存处理)
+            var fallbackResponse = new HttpResponseMessage
+            {
+                Content = new StringContent("系统正繁忙，请稍后重试"),// 内容，自定义内容
+                StatusCode = HttpStatusCode.GatewayTimeout // 504
+            };
+
             // 使用HttpClientFactory
             services.AddHttpClient();
 
@@ -39,6 +49,19 @@ namespace EPM.MVCClient
 
             services.Configure<UserServiceConfig>(Configuration.GetSection("UserServiceConfig"));
             services.Configure<DeptServiceConfig>(Configuration.GetSection("DeptServiceConfig"));
+
+            
+            PollyConfig pollyConfig= new PollyConfig();
+            Configuration.GetSection("PollyConfig").Bind(pollyConfig);
+
+            services.AddPollyHttpClient("mrico", options =>
+            {
+                options.TimeoutTime = pollyConfig.TimeoutTime; // 1、超时时间
+                options.RetryCount = pollyConfig.RetryCount;// 2、重试次数
+                options.CircuitBreakerOpenFallCount = pollyConfig.CircuitBreakerOpenFallCount;// 3、熔断器开启(多少次失败开启)
+                options.CircuitBreakerDownTime = pollyConfig.CircuitBreakerDownTime;// 4、熔断器开启时间
+                options.httpResponseMessage = fallbackResponse;// 5、降级处理
+            });
 
             // 写在最后面,否则会导致写在这行代码后面的注入获取不到
             ServiceFactory.Services = services;

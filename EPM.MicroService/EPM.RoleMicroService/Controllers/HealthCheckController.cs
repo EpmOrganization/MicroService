b273eb486701fcda98684b29916a7c1833bce32e
jiangxl <jiangxl@hicore.com>
2022-01-06 16:12:54 +0800

    添加网关聚合

---
+++ EPM.MicroService/EPM.RoleMicroService/Controllers/HealthCheckController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EPM.RoleMicroService.Controllers
+{
+    /// <summary>
+    /// 健康检查
+    /// </summary>
+    [Route("api/role/HealthCheck")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            //Console.WriteLine("UserService is ok");
+            return Ok("RoleService is ok");
+        }
+    }
+}
